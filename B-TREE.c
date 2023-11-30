55c53c6eb283be9b04e971035addeb7bec1d1801
logesh kumar <118279520+Logesh314@users.noreply.github.com>
2023-11-30 12:26:18 +0530

    Create B-TREE.c

---
+++ B-TREE.c
@@ -0,0 +1,148 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 3
+#define MIN 2
+
+struct BTreeNode {
+  int val[MAX + 1], count;
+  struct BTreeNode *link[MAX + 1];
+};
+
+struct BTreeNode *root;
+
+struct BTreeNode *createNode(int val, struct BTreeNode *child) {
+  struct BTreeNode *newNode;
+  newNode = (struct BTreeNode *)malloc(sizeof(struct BTreeNode));
+  newNode->val[1] = val;
+  newNode->count = 1;
+  newNode->link[0] = root;
+  newNode->link[1] = child;
+  return newNode;
+}
+
+
+void insertNode(int val, int pos, struct BTreeNode *node,
+        struct BTreeNode *child) {
+  int j = node->count;
+  while (j > pos) {
+    node->val[j + 1] = node->val[j];
+    node->link[j + 1] = node->link[j];
+    j--;
+  }
+  node->val[j + 1] = val;
+  node->link[j + 1] = child;
+  node->count++;
+}
+
+
+void splitNode(int val, int *pval, int pos, struct BTreeNode *node,
+         struct BTreeNode *child, struct BTreeNode **newNode) {
+  int median, j;
+
+  if (pos > MIN)
+    median = MIN + 1;
+  else
+    median = MIN;
+
+  *newNode = (struct BTreeNode *)malloc(sizeof(struct BTreeNode));
+  j = median + 1;
+  while (j <= MAX) {
+    (*newNode)->val[j - median] = node->val[j];
+    (*newNode)->link[j - median] = node->link[j];
+    j++;
+  }
+  node->count = median;
+  (*newNode)->count = MAX - median;
+
+  if (pos <= MIN) {
+    insertNode(val, pos, node, child);
+  } else {
+    insertNode(val, pos - median, *newNode, child);
+  }
+  *pval = node->val[node->count];
+  (*newNode)->link[0] = node->link[node->count];
+  node->count--;
+}
+
+
+int setValue(int val, int *pval,
+           struct BTreeNode *node, struct BTreeNode **child) {
+  int pos;
+  if (!node) {
+    *pval = val;
+    *child = NULL;
+    return 1;
+  }
+
+  if (val < node->val[1]) {
+    pos = 0;
+  } else {
+    for (pos = node->count;
+       (val < node->val[pos] && pos > 1); pos--);
+    if (val == node->val[pos]) {
+      printf("Duplicates are not permitted\n");
+      return 0;
+    }
+  }
+  if (setValue(val, pval, node->link[pos], child)) {
+    if (node->count < MAX) {
+      insertNode(*pval, pos, node, *child);
+    } else {
+      splitNode(*pval, pval, pos, node, *child, child);
+      return 1;
+    }
+  }
+  return 0;
+}
+
+
+void insert(int val) {
+  int flag, i;
+  struct BTreeNode *child;
+
+  flag = setValue(val, &i, root, &child);
+  if (flag)
+    root = createNode(i, child);
+}
+
+
+
+
+void traversal(struct BTreeNode *myNode) {
+  int i;
+  if (myNode) {
+    for (i = 0; i < myNode->count; i++) {
+      traversal(myNode->link[i]);
+      printf("%d ", myNode->val[i + 1]);
+    }
+    traversal(myNode->link[i]);
+  }
+}
+
+int main() {
+  int val, ch;
+  int choice,element;
+  while(1){
+    printf("\n********* B-TREE OF ORDER 3 ***********\n");
+    printf("1. Insert\n2. Display\n3. Exit\n");
+    printf("\nEnter your choice:");
+    scanf("%d",&choice);
+    switch(choice)
+    {
+    case 1:
+        printf("\nEnter the key to insert:");
+        scanf("%d",&element);
+        insert(element);
+        break;
+    case 2:
+        printf("\nInorder traversal:");
+        traversal(root);
+        break;
+    case 3:
+        exit(1);
+        break;
+    }
+  }
+  printf("\n");
+
+}
